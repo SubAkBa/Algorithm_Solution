4b329fed4bbddf80338464691be5d0186215c0e2
SubAkBa <circle5926@gmail.com>
2020-06-08 22:01:28 +0900

    Modify Lope Greedy Solution(2217)

--- BaekJoon/Solution_2217.java
+++ BaekJoon/Solution_2217.java
@@ -9,20 +9,17 @@ public class greedy_2217_solution {
 
 		int N = Integer.parseInt(br.readLine());
 		int[] rope = new int[N];
-		int min = 10000, weight = 0;
+		int weight = 0;
 
 		for (int i = 0; i < N; i++)
 			rope[i] = Integer.parseInt(br.readLine());
 
 		Arrays.sort(rope);
 
-		for (int i = N - 1; i >= 0; i--) {
-			min = Math.min(min, rope[i]);
-			weight = Math.max(weight, min * (N - i));
-		}
+		for (int i = N - 1; i >= 0; i--)
+			weight = Math.max(weight, rope[i] * (N - i));
 		
-		System.out.println(weight);
-		
+        bw.write(weight + "");
 		bw.flush();
 		bw.close();
 		br.close();
