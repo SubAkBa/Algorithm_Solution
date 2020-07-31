afbbc440a54029b0f17399fd20876829c0ade7a9
SubAkBa <circle5926@gmail.com>
2020-07-31 16:18:26 +0900

    Topological Sort Solution (1005 Again)

--- BaekJoon/Solution_1005.java
+++ BaekJoon/Solution_1005.java
@@ -2,16 +2,17 @@ import java.util.*;
 import java.io.*;
 
 public class Solution_1005 {
-	static ArrayList<Integer>[] edgelist;
-	static int[] indeg, time, acc_time;
-	static int N;
+	static List<Integer>[] adj;
+	static int[] D, indeg;
+	static int N, K;
 
-	public static int Build(int target) {
+	public static int ACM_Craft(int dest) {
 		Queue<Integer> queue = new LinkedList<>();
+		int[] maxtime = new int[N + 1];
 
-		for (int i = 1; i <= N; i++) {
+		for (int i = 1; i <= N; ++i) {
 			if (indeg[i] == 0) {
-				acc_time[i] = time[i];
+				maxtime[i] = D[i];
 				queue.offer(i);
 			}
 		}
@@ -19,17 +20,15 @@ public class Solution_1005 {
 		while (!queue.isEmpty()) {
 			int from = queue.poll();
 
-			for (int to : edgelist[from]) {
-				indeg[to]--;
+			for (int to : adj[from]) {
+				maxtime[to] = Math.max(maxtime[to], maxtime[from] + D[to]);
 
-				if (indeg[to] == 0)
+				if (--indeg[to] == 0)
 					queue.offer(to);
-
-				acc_time[to] = Math.max(acc_time[to], acc_time[from] + time[to]);
 			}
 		}
 
-		return acc_time[target];
+		return maxtime[dest];
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -37,37 +36,33 @@ public class Solution_1005 {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int T = Integer.parseInt(br.readLine());
 
-		while ((T--) > 0) {
+		while ((--T) >= 0) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-
 			N = Integer.parseInt(st.nextToken());
-			int K = Integer.parseInt(st.nextToken());
+			K = Integer.parseInt(st.nextToken());
+
+			D = new int[N + 1];
+			indeg = new int[N + 1];
+			adj = new ArrayList[N + 1];
 
 			st = new StringTokenizer(br.readLine());
-
-			time = new int[N + 1];
-			indeg = new int[N + 1];
-			edgelist = new ArrayList[N + 1];
-			acc_time = new int[N + 1];
-
-			for (int i = 1; i <= N; i++) {
-				time[i] = Integer.parseInt(st.nextToken());
-				edgelist[i] = new ArrayList<>();
+			for (int i = 1; i <= N; ++i) {
+				adj[i] = new ArrayList<>();
+				D[i] = Integer.parseInt(st.nextToken());
 			}
 
-			for (int i = 0; i < K; i++) {
+			for (int i = 0; i < K; ++i) {
 				st = new StringTokenizer(br.readLine());
-
 				int X = Integer.parseInt(st.nextToken());
 				int Y = Integer.parseInt(st.nextToken());
 
-				edgelist[X].add(Y);
-				indeg[Y]++;
+				adj[X].add(Y);
+				++indeg[Y];
 			}
 
-			int target = Integer.parseInt(br.readLine());
+			int dest = Integer.parseInt(br.readLine());
 
-			bw.write(Build(target) + "\n");
+			bw.write(ACM_Craft(dest) + "\n");
 		}
 
 		bw.flush();
