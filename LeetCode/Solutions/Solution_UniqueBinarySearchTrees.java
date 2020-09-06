c183e868833f370940b477bf5582280883cfd1c7
SubAkBa <circle5926@gmail.com>
2020-09-07 01:15:26 +0900

    Catalan Number DP Solution (Unique Binary Search Trees)

---
+++ LeetCode/Solutions/Solution_UniqueBinarySearchTrees.java
@@ -0,0 +1,27 @@
+
+public class Solution_UniqueBinarySearchTrees {
+	static int[][] comb = new int[39][39];
+
+	public static int Combination(int[][] comb, int n, int k) {
+		if (n == k || k == 0)
+			return comb[n][k] = 1;
+
+		if (comb[n][k] != 0)
+			return comb[n][k];
+
+		return comb[n][k] = Combination(comb, n - 1, k) + Combination(comb, n - 1, k - 1);
+	}
+
+	public static int numTrees(int n) {
+
+		return Combination(comb, 2 * n, n) - Combination(comb, 2 * n, n + 1);
+	}
+
+	public static void main(String[] args) {
+		System.out.println(numTrees(3)); // 5
+		System.out.println(numTrees(4)); // 14
+		System.out.println(numTrees(5)); // 42
+		System.out.println(numTrees(6)); // 132
+		System.out.println(numTrees(7)); // 429
+	}
+}
