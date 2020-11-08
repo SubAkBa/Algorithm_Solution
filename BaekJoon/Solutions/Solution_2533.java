6ceea2fde4f06fd3ccad251b754beac9b78a24d6
SubAkBa <circle5926@gmail.com>
2020-11-08 14:36:42 +0900

    Modify 2533

--- BaekJoon/Solutions/Solution_2533.java
+++ BaekJoon/Solutions/Solution_2533.java
@@ -3,34 +3,24 @@ import java.util.*;
 
 public class Solution_2533 {
 
-	public static void DFS(int N, List<Integer>[] adj, List<Integer>[] adj2, boolean[] visited, int from) {
+	public static int Min(int a, int b) {
+		return a < b ? a : b;
+	}
+
+	public static void DFS(int N, List<Integer>[] adj, int[][] dp, boolean[] visited, int from) {
 		visited[from] = true;
+
+		dp[from][1] = 1;
 
 		for (int to : adj[from]) {
 			if (visited[to])
 				continue;
 
-			adj2[from].add(to);
-			DFS(N, adj, adj2, visited, to);
+			DFS(N, adj, dp, visited, to);
+
+			dp[from][0] += dp[to][1];
+			dp[from][1] += Min(dp[to][0], dp[to][1]);
 		}
-	}
-
-	public static int TreeDP(int N, List<Integer>[] adj2, int[][] dp, int from, int early) {
-		if (dp[from][early] != -1)
-			return dp[from][early];
-
-		int result = 0;
-		if (early == 1) {
-			++result;
-
-			for (int to : adj2[from])
-				result += Math.min(TreeDP(N, adj2, dp, to, 1), TreeDP(N, adj2, dp, to, 0));
-		} else {
-			for (int to : adj2[from])
-				result += TreeDP(N, adj2, dp, to, 1);
-		}
-
-		return dp[from][early] = result;
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -39,13 +29,10 @@ public class Solution_2533 {
 		int N = Integer.parseInt(br.readLine());
 
 		List<Integer>[] adj = new ArrayList[N + 1];
-		List<Integer>[] adj2 = new ArrayList[N + 1];
 
-		for (int i = 0; i <= N; ++i) {
+		for (int i = 0; i <= N; ++i)
 			adj[i] = new ArrayList<>();
-			adj2[i] = new ArrayList<>();
-		}
-		
+
 		for (int i = 1; i < N; ++i) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -55,14 +42,11 @@ public class Solution_2533 {
 			adj[b].add(a);
 		}
 
-		DFS(N, adj, adj2, new boolean[N + 1], 1);
-
 		int[][] dp = new int[N + 1][2];
 
-		for (int i = 0; i <= N; ++i)
-			Arrays.fill(dp[i], -1);
+		DFS(N, adj, dp, new boolean[N + 1], 1);
 
-		bw.write(Math.min(TreeDP(N, adj2, dp, 1, 0), TreeDP(N, adj2, dp, 1, 1)) + "");
+		bw.write(Min(dp[1][1], dp[1][0]) + "");
 		bw.flush();
 		bw.close();
 		br.close();
