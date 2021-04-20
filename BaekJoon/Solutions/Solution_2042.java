bcbbb3356e394b3acda8f48a2e36300d19638331
SubAkBa <circle5926@gmail.com>
2021-04-20 23:09:42 +0900

    Segment Tree Solution (2042)

--- BaekJoon/Solutions/Solution_2042.java
+++ BaekJoon/Solutions/Solution_2042.java
@@ -3,85 +3,76 @@ import java.io.*;
 
 public class Solution_2042 {
 
-	public static int AllocSize(int num) {
-		return (int) Math.pow(2, (int) Math.ceil(Math.log(num) / Math.log(2)) + 1);
-	}
+    public static long Init(long[] nums, long[] tree, int left, int right, int node) {
+        if (left == right)
+            return tree[node] = nums[left];
 
-	public static long InitPartialSum(long[] nums, long[] tree, int node, int start, int end) {
-		if (start == end)
-			return tree[node] = nums[start];
+        int mid = (left + right) / 2;
+        return tree[node] = Init(nums, tree, left, mid, node * 2) + Init(nums, tree, mid + 1, right, node * 2 + 1);
+    }
 
-		int mid = (start + end) / 2;
-		return tree[node] = InitPartialSum(nums, tree, node * 2, start, mid)
-				+ InitPartialSum(nums, tree, node * 2 + 1, mid + 1, end);
-	}
+    public static void Update(long[] tree, int left, int right, int idx, long diff, int node) {
+        if (!(left <= idx && idx <= right))
+            return;
 
-	public static void Update(long[] tree, int node, int idx, int start, int end, long diff) {
-		if (!(start <= idx && idx <= end))
-			return;
+        tree[node] += diff;
 
-		tree[node] += diff;
+        if (left == right)
+            return;
 
-		if (start != end) {
-			int mid = (start + end) / 2;
+        int mid = (left + right) / 2;
+        Update(tree, left, mid, idx, diff, node * 2);
+        Update(tree, mid + 1, right, idx, diff, node * 2 + 1);
+    }
 
-			Update(tree, node * 2, idx, start, mid, diff);
-			Update(tree, node * 2 + 1, idx, mid + 1, end, diff);
-		}
-	}
+    public static long Query(long[] tree, int left, int right, int start, int end, int node) {
+        if (start > right || end < left)
+            return 0;
 
-	public static long PartialSum(long[] tree, int node, int start, int end, int left, int right) {
-		if (left > end || right < start)
-			return 0;
+        if (start <= left && right <= end)
+            return tree[node];
 
-		if (left <= start && end <= right)
-			return tree[node];
+        int mid = (left + right) / 2;
 
-		int mid = (start + end) / 2;
+        return Query(tree, left, mid, start, end, node * 2) + Query(tree, mid + 1, right, start, end, node * 2 + 1);
+    }
 
-		return PartialSum(tree, node * 2, start, mid, left, right)
-				+ PartialSum(tree, node * 2 + 1, mid + 1, end, left, right);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-		int size = AllocSize(N);
-		long[] nums = new long[N];
-		long[] tree = new long[size];
+        int h = (int) (Math.log(N) / Math.log(2));
+        long[] nums = new long[N + 1];
+        long[] tree = new long[1 << (h + 1) + 1];
+        for (int i = 1; i <= N; ++i)
+            nums[i] = Integer.parseInt(br.readLine());
 
-		for (int i = 0; i < N; i++)
-			nums[i] = Long.parseLong(br.readLine());
+        Init(nums, tree, 1, N, 1);
 
-		InitPartialSum(nums, tree, 1, 0, N - 1);
+        for (int i = 0; i < M + K; ++i) {
+            st = new StringTokenizer(br.readLine());
 
-		for (int i = 0; i < M + K; i++) {
-			st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-			int sep = Integer.parseInt(st.nextToken());
-			int n1 = Integer.parseInt(st.nextToken());
-			int n2 = Integer.parseInt(st.nextToken());
+            if (a == 1) {
+                long c = Long.parseLong(st.nextToken());
+                long diff = c - nums[b];
+                nums[b] = c;
+                Update(tree, 1, N, b, diff, 1);
+            } else {
+                int c = Integer.parseInt(st.nextToken());
+                bw.write(Query(tree, 1, N, b, c, 1) + "\n");
+            }
+        }
 
-			switch (sep) {
-			case 1:
-				int idx = n1 - 1;
-				long diff = n2 - nums[idx];
-				nums[idx] = n2;
-				Update(tree, 1, n1 - 1, 0, N - 1, diff);
-				break;
-			case 2:
-				bw.write(PartialSum(tree, 1, 0, N - 1, n1 - 1, n2 - 1) + "\n");
-				break;
-			}
-		}
-
-		bw.flush();
-		bw.close();
-		br.close();
-	}
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
