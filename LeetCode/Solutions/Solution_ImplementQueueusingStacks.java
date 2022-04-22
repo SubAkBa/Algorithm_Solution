703e2539ed9d9d8ca896d8445193332ccfe103a4
SubAkBa <circle5926@gmail.com>
2022-04-23 08:35:07 +0900

    Stack Solution (Implement Queue using Stacks)

--- LeetCode/Solutions/Solution_ImplementQueueusingStacks.java
+++ LeetCode/Solutions/Solution_ImplementQueueusingStacks.java
@@ -1,53 +1,50 @@
 import java.util.*;
 
 public class Solution_ImplementQueueusingStacks {
-	public static class MyQueue {
-		Stack<Integer> stack1;
-		Stack<Integer> stack2;
 
-		public MyQueue() {
-			stack1 = new Stack<>();
-			stack2 = new Stack<>();
-		}
+    public static class MyQueue {
 
-		/** Push element x to the back of queue. */
-		public void push(int x) {
-			stack1.push(x);
-		}
+        Stack<Integer> stack1, stack2;
 
-		/** Removes the element from in front of queue and returns that element. */
-		public int pop() {
-			if (stack2.isEmpty()) {
-				while (!stack1.isEmpty())
-					stack2.push(stack1.pop());
-			}
+        public MyQueue() {
+            stack1 = new Stack<>();
+            stack2 = new Stack<>();
+        }
 
-			return stack2.pop();
-		}
+        public void push(int x) {
+            stack1.push(x);
+        }
 
-		/** Get the front element. */
-		public int peek() {
-			if (stack2.isEmpty()) {
-				while (!stack1.isEmpty())
-					stack2.push(stack1.pop());
-			}
+        public int pop() {
+            if (stack2.isEmpty()) {
+                while (!stack1.isEmpty()) {
+                    stack2.push(stack1.pop());
+                }
+            }
 
-			return stack2.peek();
-		}
+            return stack2.pop();
+        }
 
-		/** Returns whether the queue is empty. */
-		public boolean empty() {
-			return stack1.isEmpty() && stack2.isEmpty();
-		}
-	}
+        public int peek() {
+            if (stack2.isEmpty()) {
+                while (!stack1.isEmpty()) {
+                    stack2.push(stack1.pop());
+                }
+            }
 
-	public static void main(String[] args) {
-		MyQueue queue = new MyQueue();
+            return stack2.peek();
+        }
 
-		queue.push(1);
-		queue.push(2);
-		System.out.println(queue.peek()); // returns 1
-		System.out.println(queue.pop()); // returns 1
-		System.out.println(queue.empty()); // returns false
-	}
+        public boolean empty() {
+            return stack1.isEmpty() && stack2.isEmpty();
+        }
+    }
+
+    public static void main(String[] args) {
+        MyQueue obj = new MyQueue();
+        obj.push(3);
+        int param_2 = obj.pop();
+        int param_3 = obj.peek();
+        boolean param_4 = obj.empty();
+    }
 }
