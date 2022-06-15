93f913ffcaa6863966f07d4894042395c7a3b9fc
SubAkBa <circle5926@gmail.com>
2022-06-15 22:13:39 +0900

    Boyer-Moore Voting Algorithm Solution (Majority Element)

--- LeetCode/Solutions/Solution_MajorityElement.java
+++ LeetCode/Solutions/Solution_MajorityElement.java
@@ -2,14 +2,38 @@ import java.util.*;
 
 public class Solution_MajorityElement {
 
-	public static int majorityElement(int[] nums) {
-		Arrays.sort(nums);
+    // O(N) / O(1)
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = Integer.MAX_VALUE;
 
-		return nums[nums.length / 2];
-	}
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
 
-	public static void main(String[] args) {
-		System.out.println(majorityElement(new int[] { 3, 2, 3 })); // 3
-		System.out.println(majorityElement(new int[] { 2, 2, 1, 1, 1, 2, 2 })); // 2
-	}
+            count += (candidate == num) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+
+    // O(N) / O(N)
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        int majority = 0, half = (int) Math.floor(nums.length / 2.0);
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > half) {
+                majority = entry.getKey();
+                break;
+            }
+        }
+
+        return majority;
+    }
 }
