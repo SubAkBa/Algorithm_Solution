dd78b8b12e07f4979bac562beff0b7c2cce07f1c
SubAkBa <circle5926@gmail.com>
2025-09-06 14:23:16 +0900

    BaekJoon Solution - dijkstra

--- BaekJoon/Solutions/Solution_11779.java
+++ BaekJoon/Solutions/Solution_11779.java
@@ -4,89 +4,89 @@ import java.io.*;
 public class Solution_11779 {
     static int INF = 987654321;
 
-    public static class Node implements Comparable<Node> {
-        int idx, cost;
+    public static class Edge implements Comparable<Edge> {
+        int node, weight;
 
-        public Node(int to, int cost) {
-            this.idx = to;
-            this.cost = cost;
+        public Edge(int node, int weight) {
+            this.node = node;
+            this.weight = weight;
         }
 
         @Override
-        public int compareTo(Node o) {
-            return this.cost - o.cost;
+        public int compareTo(Edge o) {
+            return this.weight - o.weight;
         }
     }
 
-    public static void Dijkstra(List<Node>[] adj, int n, int start, int end) {
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        int[] dist = new int[n + 1];
-        int[] path = new int[n + 1];
+    public static void dijkstra(int start, List<Edge>[] adjList, int[] weights, int[] path) {
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
 
-        Arrays.fill(dist, INF);
-
-        dist[start] = 0;
-        path[start] = -1;
-        pq.offer(new Node(start, dist[start]));
+        pq.offer(new Edge(start, 0));
+        weights[start] = 0;
+        path[start] = start;
 
         while (!pq.isEmpty()) {
-            Node current = pq.poll();
+            Edge current = pq.poll();
 
-            if (current.cost > dist[current.idx])
+            if (current.weight != weights[current.node]) {
                 continue;
-
-            for (Node to : adj[current.idx]) {
-                if (dist[to.idx] < dist[current.idx] + to.cost)
-                    continue;
-
-                path[to.idx] = current.idx;
-                dist[to.idx] = dist[current.idx] + to.cost;
-                pq.offer(new Node(to.idx, dist[to.idx]));
             }
-        }
 
-        System.out.println(dist[end]);
-        List<Integer> paths = new ArrayList<>();
-
-        int p = end;
-        while (p != -1) {
-            paths.add(p);
-            p = path[p];
-        }
-
-        int len = paths.size();
-        System.out.println(len);
-        for (int i = len - 1; i >= 0; --i) {
-            System.out.print(paths.get(i) + " ");
+            for (Edge next : adjList[current.node]) {
+                if (weights[next.node] > weights[current.node] + next.weight) {
+                    path[next.node] = current.node;
+                    weights[next.node] = weights[current.node] + next.weight;
+                    pq.offer(new Edge(next.node, weights[next.node]));
+                }
+            }
         }
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
-        List<Node>[] adj = new ArrayList[n + 1];
+        List<Edge>[] adjList = new ArrayList[n + 1];
+        int[] weights = new int[n + 1];
+        int[] path = new int[n + 1];
 
-        for (int i = 1; i <= n; ++i)
-            adj[i] = new ArrayList<>();
+        for (int i = 1; i <= n; ++i) {
+            adjList[i] = new ArrayList<>();
+            weights[i] = INF;
+        }
 
         for (int i = 0; i < m; ++i) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
 
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
 
-            adj[a].add(new Node(b, c));
+            adjList[a].add(new Edge(b, c));
         }
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
 
         int start = Integer.parseInt(st.nextToken());
         int end = Integer.parseInt(st.nextToken());
 
-        Dijkstra(adj, n, start, end);
+        dijkstra(start, adjList, weights, path);
 
-        br.close();
+        int count = 1;
+        System.out.println(weights[end]);
+
+        List<Integer> answerPath = new ArrayList<>();
+        answerPath.add(end);
+        for (int i = end; i != start; i = path[i]) {
+            ++count;
+            answerPath.add(path[i]);
+        }
+        System.out.println(count);
+
+        for (int i = answerPath.size() - 1; i >= 0; --i) {
+            System.out.print(answerPath.get(i) + " ");
+        }
     }
 }
