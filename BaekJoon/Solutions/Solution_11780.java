73188ba512b27dbda889474cf262e6fa66d1e434
SubAkBa <circle5926@gmail.com>
2025-09-07 11:29:16 +0900

    BaekJoon Solution - floyd-warshall

--- BaekJoon/Solutions/Solution_11780.java
+++ BaekJoon/Solutions/Solution_11780.java
@@ -2,83 +2,86 @@ import java.util.*;
 import java.io.*;
 
 public class Solution_11780 {
-    static int[][] graph, path;
-    static int n, m, INF = 987654321;
+    static int INF = 987654321;
 
-    public static void Floyd_Warshall() {
-        for (int via = 1; via <= n; ++via) {
-            for (int from = 1; from <= n; ++from) {
-                for (int to = 1; to <= n; ++to) {
-                    if (from == to)
+    public static void floydWarshall(int N, int[][] dist, int[][] next) {
+        for (int k = 1; k <= N; ++k) {
+            for (int i = 1; i <= N; ++i) {
+                if (dist[i][k] == INF) {
+                    continue;
+                }
+
+                for (int j = 1; j <= N; ++j) {
+                    if (i == j || dist[k][j] == INF) {
                         continue;
+                    }
 
-                    if (graph[from][to] > graph[from][via] + graph[via][to]) {
-                        graph[from][to] = graph[from][via] + graph[via][to];
-                        path[from][to] = via;
+                    if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                        next[i][j] = next[i][k];
                     }
                 }
             }
         }
     }
 
-    public static void FindPath(List<Integer> pathList, int start, int end) {
-        if (path[start][end] == 0) {
-            pathList.add(start);
-            pathList.add(end);
-            return;
-        }
-        FindPath(pathList, start, path[start][end]);
-        pathList.remove(pathList.size() - 1);
-        FindPath(pathList, path[start][end], end);
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        n = Integer.parseInt(br.readLine());
-        m = Integer.parseInt(br.readLine());
-        graph = new int[n + 1][n + 1];
-        path = new int[n + 1][n + 1];
+        StringTokenizer st;
 
-        for (int i = 1; i <= n; ++i)
-            Arrays.fill(graph[i], INF);
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        int[][] dist = new int[n + 1][n + 1];
+        int[][] next = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; ++i) {
+            Arrays.fill(dist[i], INF);
+            dist[i][i] = 0;
+        }
 
         for (int i = 0; i < m; ++i) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
+
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
 
-            graph[a][b] = (graph[a][b] == 0 ? c : Math.min(graph[a][b], c));
+            if (dist[a][b] > c) {
+                dist[a][b] = c;
+                next[a][b] = b;
+            }
         }
 
-        Floyd_Warshall();
+        floydWarshall(n, dist, next);
 
+        StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j <= n; ++j)
-                bw.write((i == j ? 0 : graph[i][j]) + " ");
-            bw.newLine();
+            for (int j = 1; j <= n; ++j) {
+                sb.append(dist[i][j] == INF ? 0 : dist[i][j]).append(" ");
+            }
+            sb.append("\n");
         }
 
         for (int i = 1; i <= n; ++i) {
             for (int j = 1; j <= n; ++j) {
-                if (graph[i][j] == INF)
-                    bw.write("0");
-                else {
-                    List<Integer> pathList = new ArrayList<>();
-                    FindPath(pathList, i, j);
-                    bw.write(pathList.size() + " ");
+                if (i == j || next[i][j] == 0) {
+                    sb.append("0\n");
+                } else {
+                    List<Integer> path = new ArrayList<>();
+                    for (int k = i; k != j; k = next[k][j]) {
+                        path.add(k);
+                    }
+                    path.add(j);
 
-                    for (int p : pathList)
-                        bw.write(p + " ");
+                    sb.append(path.size());
+                    for (int p : path) {
+                        sb.append(" ").append(p);
+                    }
+                    sb.append("\n");
                 }
-
-                bw.newLine();
             }
         }
 
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(sb.toString());
     }
 }
