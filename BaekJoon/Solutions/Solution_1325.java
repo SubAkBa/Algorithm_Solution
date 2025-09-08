5768c5596e1c28ea8fc18d7831adeeab05388db0
SubAkBa <circle5926@gmail.com>
2025-09-09 00:44:10 +0900

    BaekJoon Solution - bfs

--- BaekJoon/Solutions/Solution_1325.java
+++ BaekJoon/Solutions/Solution_1325.java
@@ -1,65 +1,75 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Solution_1325 {
-	static ArrayList<Integer>[] edgelist;
-	static int N;
-	static int[] count;
-	static boolean[] visited;
+	public static void bfs(int start, int visitId, List<Integer>[] adjList, int[] dist, int[] seen) {
+		Queue<Integer> queue = new ArrayDeque<>();
+		int count = 0;
 
-	public static void Hacking(int start) {
-		visited[start] = true;
+		queue.offer(start);
+		seen[start] = visitId;
 
-		for (int next : edgelist[start]) {
-			if (!visited[next]) {
-				count[next]++;
-				Hacking(next);
+		while (!queue.isEmpty()) {
+			int current = queue.poll();
+
+			for (int next : adjList[current]) {
+				if (seen[next] != visitId) {
+					seen[next] = visitId;
+					queue.offer(next);
+					++count;
+				}
 			}
 		}
+
+		dist[start] = count;
 	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
+
+		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
+		List<Integer>[] adj = new ArrayList[N + 1];
+		int[] dist = new int[N + 1];
+		int[] seen = new int[N + 1];
 
-		edgelist = new ArrayList[N + 1];
-		visited = new boolean[N + 1];
-		count = new int[N + 1];
+		for (int i = 1; i <= N; ++i) {
+			adj[i] = new ArrayList<>();
+		}
 
-		for (int i = 1; i <= N; i++)
-			edgelist[i] = new ArrayList<>();
-
-		for (int i = 0; i < M; i++) {
+		for (int i = 0; i < M; ++i) {
 			st = new StringTokenizer(br.readLine());
 
 			int A = Integer.parseInt(st.nextToken());
 			int B = Integer.parseInt(st.nextToken());
-
-			edgelist[A].add(B);
+			adj[B].add(A);
 		}
 
-		int max = 0;
-		for (int i = 1; i <= N; i++) {
-			Arrays.fill(visited, false);
-			Hacking(i);
+		int visitId = 0;
+		for (int i = 1; i <= N; ++i) {
+			++visitId;
+			bfs(i, visitId, adj, dist, seen);
 		}
 
-		for (int i = 1; i <= N; i++)
-			max = Math.max(max, count[i]);
+		int maxDist = 0;
+		for (int i = 1; i <= N; ++i) {
+			maxDist = Math.max(maxDist, dist[i]);
+		}
 
 		StringBuilder sb = new StringBuilder();
-		for (int i = 1; i <= N; i++) {
-			if (max == count[i])
+		for (int i = 1; i <= N; ++i) {
+			if (maxDist == dist[i]) {
 				sb.append(i + " ");
+			}
 		}
 
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-		br.close();
+		System.out.println(sb);
 	}
-
 }
--- BaekJoon/Solutions/Solution_18352.java
+++ BaekJoon/Solutions/Solution_18352.java
@@ -2,68 +2,64 @@ import java.util.*;
 import java.io.*;
 
 public class Solution_18352 {
-    static int INF = 987654321;
+    public static void function(int X, List<Integer>[] adjList, int[] dist) {
+        Queue<Integer> pq = new LinkedList<>();
 
-    public static int[] Dijkstra(List<Integer>[] adj, int X, int[] dist) {
-        Queue<Integer> queue = new LinkedList<>();
-
+        pq.offer(X);
         dist[X] = 0;
-        queue.offer(X);
 
-        while (!queue.isEmpty()) {
-            int current = queue.poll();
+        while (!pq.isEmpty()) {
+            int current = pq.poll();
 
-            for (int to : adj[current]) {
-                if (dist[to] <= dist[current] + 1)
-                    continue;
-
-                dist[to] = dist[current] + 1;
-                queue.offer(to);
+            for (int next : adjList[current]) {
+                if (dist[next] > dist[current] + 1) {
+                    dist[next] = dist[current] + 1;
+                    pq.offer(next);
+                }
             }
         }
-
-        return dist;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int INF = 987654321;
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
         int X = Integer.parseInt(st.nextToken());
 
-        List<Integer>[] adj = new ArrayList[N + 1];
+        List<Integer>[] adjList = new ArrayList[N + 1];
         int[] dist = new int[N + 1];
 
-        Arrays.fill(dist, INF);
-
-        for (int i = 1; i <= N; ++i)
-            adj[i] = new ArrayList<>();
+        for (int i = 1; i <= N; ++i) {
+            adjList[i] = new ArrayList<>();
+            dist[i] = INF;
+        }
 
         for (int i = 0; i < M; ++i) {
             st = new StringTokenizer(br.readLine());
+
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
 
-            adj[A].add(B);
+            adjList[A].add(B);
         }
 
-        Dijkstra(adj, X, dist);
+        function(X, adjList, dist);
 
-        boolean flag = false;
+        StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= N; ++i) {
             if (dist[i] == K) {
-                flag = true;
-                bw.write(i + "\n");
+                sb.append(i).append("\n");
             }
         }
 
-        if (!flag)
-            bw.write(-1 + "");
-        bw.flush();
-        bw.close();
-        br.close();
+        if (sb.length() == 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(sb);
+        }
     }
 }
