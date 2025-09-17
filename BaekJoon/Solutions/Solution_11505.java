52fa2311cf5380c4b6998df0bc68f7de48920c7f
SubAkBa <circle5926@gmail.com>
2025-09-17 23:42:16 +0900

    BaekJoon Solution - Segment Tree

--- BaekJoon/Solutions/Solution_11505.java
+++ BaekJoon/Solutions/Solution_11505.java
@@ -1,85 +1,96 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Solution_11505 {
-	static int MOD = 1000000007;
+	static long MOD = 1_000_000_007L;
+	static long[] nums, trees;
 
-	public static int AllocSize(int n) {
-		return (int) Math.pow(2, Math.ceil(Math.log(n) / Math.log(2)) + 1);
+	public static long build(int start, int end, int node) {
+		if (start == end) {
+			return trees[node] = nums[start];
+		}
+
+		int mid = (start + end) >>> 1;
+		int L = node << 1;
+		int R = L | 1;
+
+		return trees[node] = (build(start, mid, L) * build(mid + 1, end, R)) % MOD;
 	}
 
-	public static long Init(int[] nums, long[] tree, int node, int start, int end) {
-		if (start == end)
-			return tree[node] = nums[start];
+	public static void update(int start, int end, int node, int idx, long val) {
+		if (idx < start || end < idx) {
+			return;
+		}
 
-		int mid = (start + end) / 2;
+		if (start == end) {
+			trees[node] = val % MOD;
+			return;
+		}
 
-		return tree[node] = (Init(nums, tree, node * 2, start, mid) % MOD * Init(nums, tree, node * 2 + 1, mid + 1, end)
-				% MOD) % MOD;
+		int mid = (start + end) >>> 1;
+		int L = node << 1;
+		int R = L | 1;
+
+		if (idx <= mid) {
+			update(start, mid, L, idx, val);
+		} else {
+			update(mid + 1, end, R, idx, val);
+		}
+
+		trees[node] = (trees[L] * trees[R]) % MOD;
 	}
 
-	public static long Query(long[] tree, int start, int end, int node, int left, int right) {
-		if (end < left || start > right)
+	public static long query(int start, int end, int node, int left, int right) {
+		if (right < start || end < left) {
 			return 1;
+		}
 
-		if (left <= start && end <= right)
-			return tree[node];
+		if (left <= start && end <= right) {
+			return trees[node];
+		}
 
-		int mid = (start + end) / 2;
+		int mid = (start + end) >>> 1;
+		int L = node << 1;
+		int R = L | 1;
 
-		return Query(tree, start, mid, node * 2, left, right) * Query(tree, mid + 1, end, node * 2 + 1, left, right)
-				% MOD;
-	}
-
-	public static long Update(long[] tree, int start, int end, int node, int idx, long diff) {
-		if (!(start <= idx && idx <= end))
-			return tree[node];
-
-		if (start == end)
-			return tree[node] = diff;
-
-		int mid = (start + end) / 2;
-		return tree[node] = Update(tree, start, mid, node * 2, idx, diff)
-				* Update(tree, mid + 1, end, node * 2 + 1, idx, diff) % MOD;
+		return (query(start, mid, L, left, right) * query(mid + 1, end, R, left, right)) % 1000000007;
 	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
+		int treeSize = Integer.highestOneBit(N) << 2;
 
-		int size = AllocSize(N);
-		int[] nums = new int[N];
-		long[] tree = new long[size];
+		nums = new long[N + 1];
+		trees = new long[treeSize];
 
-		for (int i = 0; i < N; i++)
+		for (int i = 1; i <= N; ++i) {
 			nums[i] = Integer.parseInt(br.readLine());
+		}
 
-		Init(nums, tree, 1, 0, N - 1);
+		build(1, N, 1);
 
-		for (int i = 0; i < M + K; i++) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < M + K; ++i) {
 			st = new StringTokenizer(br.readLine());
 
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
 
-			switch (a) {
-			case 1:
-				Update(tree, 0, N - 1, 1, b - 1, c);
-				break;
-			case 2:
-				bw.write(Query(tree, 0, N - 1, 1, b - 1, c - 1) + "\n");
-				break;
+			if (a == 1) {
+				update(1, N, 1, b, c);
+			} else if (a == 2) {
+				sb.append(query(1, N, 1, b, c)).append("\n");
 			}
 		}
 
-		bw.flush();
-		bw.close();
-		br.close();
+		System.out.println(sb);
 	}
-
 }
--- BaekJoon/Solutions/Solution_2357.java
+++ BaekJoon/Solutions/Solution_2357.java
@@ -1,75 +1,81 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Solution_2357 {
-	public static int AllocSize(int n) {
-		return (int) Math.pow(2, Math.ceil(Math.log(n) / Math.log(2)) + 1);
+	static long[][] trees;
+	static long[] nums;
+
+	public static int getTreeSize(int N) {
+		return Integer.highestOneBit(N) << 2;
 	}
 
-	public static long Init(long[] nums, long[] tree, int node, int start, int end, int p) {
-		if (start == end)
-			return tree[node] = nums[start];
-
-		int mid = (start + end) / 2;
-
-		if (p == 0)
-			return tree[node] = Math.min(Init(nums, tree, node * 2, start, mid, 0),
-					Init(nums, tree, node * 2 + 1, mid + 1, end, 0));
-		else
-			return tree[node] = Math.max(Init(nums, tree, node * 2, start, mid, 1),
-					Init(nums, tree, node * 2 + 1, mid + 1, end, 1));
-	}
-
-	public static long Query(long[] tree, int node, int start, int end, int left, int right, int p) {
-		if (right < start || left > end) {
-			if (p == 0)
-				return Long.MAX_VALUE;
-			else
-				return Long.MIN_VALUE;
+	public static void build(int start, int end, int node) {
+		if (start == end) {
+			trees[node][0] = trees[node][1] = nums[start];
+			return;
 		}
 
-		if (left <= start && end <= right)
-			return tree[node];
+		int mid = (start + end) >>> 1;
+		int L = node << 1;
+		int R = L | 1;
 
-		int mid = (start + end) / 2;
+		build(start, mid, L);
+		build(mid + 1, end, R);
 
-		if (p == 0)
-			return Math.min(Query(tree, node * 2, start, mid, left, right, 0),
-					Query(tree, node * 2 + 1, mid + 1, end, left, right, 0));
-		else
-			return Math.max(Query(tree, node * 2, start, mid, left, right, 1),
-					Query(tree, node * 2 + 1, mid + 1, end, left, right, 1));
+		trees[node][0] = Math.min(trees[L][0], trees[R][0]);
+		trees[node][1] = Math.max(trees[L][1], trees[R][1]);
+	}
+
+	public static long[] getMinMax(int start, int end, int node, int left, int right) {
+		if (right < start || end < left) {
+			return new long[] {Long.MAX_VALUE, Long.MIN_VALUE};
+		}
+
+		if (left <= start && end <= right) {
+			return trees[node];
+		}
+
+		int mid = (start + end) >>> 1;
+		int L = node << 1;
+		int R = L | 1;
+
+		long[] leftArray = getMinMax(start, mid, L, left, right);
+		long[] rightArray = getMinMax(mid + 1, end, R, left, right);
+
+		return new long[] {Math.min(leftArray[0], rightArray[0]), Math.max(leftArray[1], rightArray[1])};
 	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		int size = AllocSize(N);
-		long[] nums = new long[N];
-		long[] minTree = new long[size], maxTree = new long[size];
+		int treeSize = getTreeSize(N);
 
-		for (int i = 0; i < N; i++)
-			nums[i] = Integer.parseInt(br.readLine());
+		nums = new long[N + 1];
+		trees = new long[treeSize + 1][2];
 
-		Init(nums, minTree, 1, 0, N - 1, 0);
-		Init(nums, maxTree, 1, 0, N - 1, 1);
+		for (int i = 1; i <= N; ++i) {
+			nums[i] = Long.parseLong(br.readLine());
+		}
 
-		for (int i = 0; i < M; i++) {
+		build(1, N, 1);
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < M; ++i) {
 			st = new StringTokenizer(br.readLine());
 
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 
-			bw.write(Query(minTree, 1, 0, N - 1, a - 1, b - 1, 0) + " " + Query(maxTree, 1, 0, N - 1, a - 1, b - 1, 1)
-					+ "\n");
+			long[] minMax = getMinMax(1, N, 1, a, b);
+
+			sb.append(minMax[0]).append(" ").append(minMax[1]).append("\n");
 		}
 
-		bw.flush();
-		bw.close();
-		br.close();
+		System.out.println(sb);
 	}
-
 }
